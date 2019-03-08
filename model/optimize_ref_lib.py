928b9290d3d475e99de6d5d51a580d7d59002d36
Ryan Norfield <rnorfield@gmail.com>
2019-03-07 18:44:50 -0800

    learning kivy

---
+++ model/optimize_ref_lib.py
@@ -0,0 +1,11 @@
+from ModelClassifier import ModelClassifier
+import os
+import pandas as pd
+import csv
+
+def optimize(file):
+
+
+if __name__ == '__main__':
+    optimize('./hist_data.csv')
+    # read_from_file()
