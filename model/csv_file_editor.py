08baafb52ccff8998737b1bd488c51a9773f4d0b
Ryan Norfield <rnorfield@gmail.com>
2019-03-08 10:34:43 -0800

    optimiziation algorithm
    in progress

--- model/csv_file_editor.py
+++ model/csv_file_editor.py
@@ -5,32 +5,33 @@ import csv
 import numpy as np
 
 
-def write_to_file(open_type, file_location):
-    """
-    Download files to the scans folder before running. This will allow for more objects to be classified
-    and for more precise comparisons
+# def write_to_file(open_type, file_location):
+#     """
+#     [OLD] - used to write files with the full obj data, which was highly inefficient.
+#     Download files to the scans folder before running. This will allow for more objects to be classified
+#     and for more precise comparisons
+#
+#     Disclaimer: This function will take a long time to run!
+#     :return:
+#     """
+#     temp_list = []
+#     training_files = os.listdir(file_location)
+#     for file in training_files:
+#         test_model = ModelClassifier(
+#             os.path.join(os.path.join(os.path.dirname(__file__), file_location.strip("./")), file))
+#
+#         hist_data = test_model.generate_distribution_data(test_model.mesh_object.vertices)
+#         shape_name = file.split("_")
+#         temp_list.append([shape_name[0], ','.join(map(str, hist_data))])
+#         print(shape_name[0], "added")
+#
+#     with open("object_data.csv", open_type) as training_data:
+#         writer = csv.writer(training_data)
+#         for line in temp_list:
+#             writer.writerow(line)
 
-    Disclaimer: This function will take a long time to run!
-    :return:
-    """
-    temp_list = []
-    training_files = os.listdir(file_location)
-    for file in training_files:
-        test_model = ModelClassifier(
-            os.path.join(os.path.join(os.path.dirname(__file__), file_location.strip("./")), file))
 
-        hist_data = test_model.generate_distribution_data(test_model.mesh_object.vertices)
-        shape_name = file.split("_")
-        temp_list.append([shape_name[0], ','.join(map(str, hist_data))])
-        print(shape_name[0], "added")
-
-    with open("object_data.csv", open_type) as training_data:
-        writer = csv.writer(training_data)
-        for line in temp_list:
-            writer.writerow(line)
-
-
-def hist_to_file(open_type, file_location):
+def hist_to_file(file_location):
     """
     An attempt to save data used for comparison histograms
     :return:
@@ -58,18 +59,17 @@ def hist_to_file(open_type, file_location):
             wr.writerow(eachList)
 
 
-def read_from_file():
-    """
-    This is just a test function to determine how the file data will be manipulated or
-    if the data was stored correctly
-    :return:
-    """
-    with open("object_data.csv", 'r') as data:
-        file_data = pd.read_csv(data, header=None)
-        for i in list(file_data.values):
-            print(i[0])
+# def read_from_file():
+#     """
+#     This is just a test function to determine how the file data will be manipulated or
+#     if the data was stored correctly
+#     :return:
+#     """
+#     with open("object_data.csv", 'r') as data:
+#         file_data = pd.read_csv(data, header=None)
+#         for i in list(file_data.values):
+#             print(i[0])
 
 
 if __name__ == '__main__':
-    hist_to_file('w', './training')
-    # read_from_file()
+    hist_to_file('./training')
